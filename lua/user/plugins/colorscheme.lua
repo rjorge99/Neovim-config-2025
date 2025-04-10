eedaf82cd956cb692385abb07fe038a9ecde156a
Jorge Reyes <rjorge99@hotmail.com>
2025-04-10 14:37:26 -0600

    Se agrega color

--- lua/user/plugins/colorscheme.lua
+++ lua/user/plugins/colorscheme.lua
@@ -2,33 +2,49 @@ return {
     {
         "bluz71/vim-nightfly-guicolors",
         prority = 1000,
-        enabled = false,
+        lazy = true,
         config = function()
-            vim.cmd([[colorscheme nightfly]])
+            --vim.cmd([[colorscheme nightfly]])
         end,
     },
     {
         "Mofiqul/vscode.nvim",
         priority = 1000,
-        enabled = false,
+        lazy = true,
         config = function()
-            vim.cmd([[colorscheme vscode]])
+            --vim.cmd([[colorscheme vscode]])
         end,
     },
     {
         "rjorge99/halcyon-neovim",
         priority = 1000,
-        --enabled = false,
+        lazy = true,
         config = function()
-            vim.cmd([[colorscheme halcyon]])
+            --vim.cmd([[colorscheme halcyon]])
+        end,
+    },
+    {
+        "folke/tokyonight.nvim",
+        priority = 1000,
+        lazy = true,
+        config = function()
+            --    vim.cmd([[colorscheme tokyonight]])
         end,
     },
     {
         "rose-pine/neovim",
         priority = 1000,
-        enabled = false,
+        lazy = true,
         config = function()
-            vim.cmd([[colorscheme rose-pine]])
+            --vim.cmd([[colorscheme rose-pine]])
+        end,
+    },
+    {
+        "catppuccin/nvim",
+        priority = 1000,
+        --lazy = true,
+        config = function()
+            vim.cmd([[colorscheme catppuccin-mocha]])
         end,
     },
 }
